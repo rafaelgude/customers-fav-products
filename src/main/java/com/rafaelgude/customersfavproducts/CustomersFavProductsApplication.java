53a16f992e40bb663f5ee2135da9f19a5f3a66b7
Rafael Gude <gudemvl@hotmail.com>
2021-07-15 21:05:18 -0300

    Project started

---
+++ src/main/java/com/rafaelgude/customersfavproducts/CustomersFavProductsApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelgude.customersfavproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersFavProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersFavProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelgude/customersfavproducts/CustomersFavProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelgude.customersfavproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersFavProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
