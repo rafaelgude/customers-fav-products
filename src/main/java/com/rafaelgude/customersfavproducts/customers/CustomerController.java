9da28082ae54322f463d580a8775b42304fbd1d7
Rafael Gude <gudemvl@hotmail.com>
2021-07-21 12:17:23 -0300

    Swagger

--- src/main/java/com/rafaelgude/customersfavproducts/customers/CustomerController.java
+++ src/main/java/com/rafaelgude/customersfavproducts/customers/CustomerController.java
@@ -1,6 +1,7 @@
 package com.rafaelgude.customersfavproducts.customers;
 
 import com.rafaelgude.customersfavproducts.config.BaseRestController;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,16 +16,19 @@ public class CustomerController extends BaseRestController {
     @Autowired
     private CustomerService customerService;
 
+    @Operation(summary = "Get customer by Id without favorite products")
     @GetMapping("/{id}")
     public ResponseEntity<Customer> findById(@PathVariable Long id) {
         return ResponseEntity.ok(customerService.findById(id));
     }
 
+    @Operation(summary = "Get all customers without favorite products")
     @GetMapping
     public ResponseEntity<List<Customer>> findAll() {
         return ResponseEntity.ok(customerService.findAll());
     }
 
+    @Operation(summary = "Create a new customer")
     @PostMapping
     public ResponseEntity<CustomerDTO> create(@RequestBody @Valid CreateUpdateCustomerDTO createUpdateCustomerDTO) {
         var customerDto = customerService.create(createUpdateCustomerDTO.toEntity());
@@ -32,23 +36,27 @@ public class CustomerController extends BaseRestController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing customer")
     public ResponseEntity<CustomerDTO> update(@PathVariable Long id, @RequestBody @Valid CreateUpdateCustomerDTO createUpdateCustomerDTO) {
         return ResponseEntity.ok(customerService.update(id, createUpdateCustomerDTO.toEntity()));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete an existing customer")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         customerService.delete(id);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/{id}/add-favorite-products")
+    @Operation(summary = "Add favorite products to an existing customer")
     public ResponseEntity<Void> addFavoriteProducts(@PathVariable Long id, @RequestBody List<String> favoriteProductsId) {
         customerService.addFavoriteProducts(id, favoriteProductsId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}/with-favorite-products")
+    @Operation(summary = "Get customer by Id with favorite products")
     public ResponseEntity<CustomerWithFavoriteProductsDTO> findByIdWithFavoriteProducts(@PathVariable Long id) {
         return ResponseEntity.ok(customerService.findByIdWithFavoriteProducts(id));
     }
